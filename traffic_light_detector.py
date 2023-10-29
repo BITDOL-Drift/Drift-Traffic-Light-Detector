8842816fb040d960b1c63ca7c1768310a7a9d6cc
Jio Choi <109258000+GeoChoi@users.noreply.github.com>
2023-10-30 00:23:33 +0900

    Update traffic_light_detector.py

--- traffic_light_detector.py
+++ traffic_light_detector.py
@@ -13,8 +13,25 @@ from std_msgs.msg import Header
 
 RED = 0
 GREEN = 1
-UNKNOWN = 2
+UNKNOWN = 0
 
+#color range
+lower_red1 = np.array([0,100,100])
+upper_red1 = np.array([10,255,255])
+lower_red2 = np.array([160,100,100])
+upper_red2 = np.array([180,255,255])
+
+# lower_green = np.array([55,50,50])
+# upper_green = np.array([90,255,255])
+
+lower_green = np.array([60,50,130])
+upper_green = np.array([90,255,255])
+
+# lower_green = np.array([70,70,70])
+# upper_green = np.array([85,255,255])
+
+lower_yellow = np.array([15,150,150])
+upper_yellow = np.array([35,255,255])
 
 class BBox(object):
 	def __init__(self, x, y, width, height):
@@ -22,7 +39,6 @@ class BBox(object):
 		self.y = y
 		self.width = width
 		self.height = height
-
 
 def calculate_bounds(bound):
 	xmin = sys.maxint
@@ -72,21 +88,14 @@ def detect_callback(image):
 	# size = cimg.shape
 	# cimg = cimg[0:int(size[0]/2), :]
 	hsv = cv2.cvtColor(cimg, cv2.COLOR_BGR2HSV)
+	hsv = hsv[90:130, 150:180]
 
-	# color range
-	lower_red1 = np.array([0,100,100])
-	upper_red1 = np.array([10,255,255])
-	lower_red2 = np.array([160,100,100])
-	upper_red2 = np.array([180,255,255])
-	lower_green = np.array([40,50,50])
-	upper_green = np.array([90,255,255])
-	lower_yellow = np.array([15,150,150])
-	upper_yellow = np.array([35,255,255])
-	maskr_1 = cv2.inRange(hsv, lower_red1, upper_red1)
-	maskr_2 = cv2.inRange(hsv, lower_red2, upper_red2)
-	maskr = cv2.add(maskr_1, maskr_2)
+	# color masking
+	mask1 = cv2.inRange(hsv, lower_red1, upper_red1)
+	mask2 = cv2.inRange(hsv, lower_red2, upper_red2)
 	maskg = cv2.inRange(hsv, lower_green, upper_green)
 	masky = cv2.inRange(hsv, lower_yellow, upper_yellow)
+	maskr = cv2.add(mask1, mask2)
 
 	# kernel = np.ones((3, 3), np.uint8)
 	# maskg = cv2.erode(maskg, kernel, iterations=6)
@@ -95,42 +104,44 @@ def detect_callback(image):
 	size = hsv.shape
 
 	# hough circle detect
-	r_circles = cv2.HoughCircles(maskr, cv2.HOUGH_GRADIENT, 1, 80,
-							   param1=50, param2=5, minRadius=0, maxRadius=30)
+	# r_circles = cv2.HoughCircles(maskr, cv2.HOUGH_GRADIENT, 1, 90,
+	# 						   param1=50, param2=5, minRadius=30, maxRadius=50)
 
 	g_circles = cv2.HoughCircles(maskg, cv2.HOUGH_GRADIENT, 1, 50,
-								 param1=50, param2=5, minRadius=0, maxRadius=30)
+								 param1=50, param2=5, minRadius=10, maxRadius=30)
 
-	y_circles = cv2.HoughCircles(masky, cv2.HOUGH_GRADIENT, 1, 30,
-								 param1=50, param2=5, minRadius=0, maxRadius=30)
+	# y_circles = cv2.HoughCircles(masky, cv2.HOUGH_GRADIENT, 1, 30,
+	# 							 param1=50, param2=5, minRadius=0, maxRadius=30)
 
 	# traffic light detect
 	red_detected = False
 	green_detected = False
 	r = 5
 	bound = 4.0 / 10
-	if r_circles is not None and not green_detected:
-		bound = 4.0 / 10
-		r_circles = np.uint16(np.around(r_circles))
+	# if r_circles is not None and not green_detected:
+	# 	bound = 4.0 / 10
+	# 	r_circles = np.uint16(np.around(r_circles))
 
-		for i in r_circles[0, :]:
-			if i[0] > size[1] or i[1] > size[0]or i[1] > size[0]*bound:
-				continue
+	# 	for i in r_circles[0, :]:
+	# 		if i[0] > size[1] or i[1] > size[0]or i[1] > size[0]*bound:
+	# 			continue
 
-			h, s = 0.0, 0.0
-			for m in range(-r, r):
-				for n in range(-r, r):
+	# 		h, s = 0.0, 0.0
+	# 		for m in range(-r, r):
+	# 			for n in range(-r, r):
 
-					if (i[1]+m) >= size[0] or (i[0]+n) >= size[1]:
-						continue
-					h += maskr[i[1]+m, i[0]+n]
-					s += 1
-			if h / s > 50:
-				cv2.circle(cimg, (i[0], i[1]), i[2]+10, (0, 0, 255), 2)
-				traffic_light_result.recognition_result = RED
-				red_detected = True
+	# 				if (i[1]+m) >= size[0] or (i[0]+n) >= size[1]:
+	# 					continue
+	# 				h += maskr[i[1]+m, i[0]+n]
+	# 				s += 1
+	# 		if h / s > 50:
+	# 			cv2.circle(cimg, (i[0], i[1]), i[2]+10, (0, 0, 255), 2)
+	# 			traffic_light_result.recognition_result = RED
+	# 			red_detected = True
+		
 	
-	if g_circles is not None and not red_detected:
+	# if g_circles is not None and not red_detected:
+	if g_circles is not None:
 		g_circles = np.uint16(np.around(g_circles))
 
 		for i in g_circles[0, :]:
@@ -150,10 +161,14 @@ def detect_callback(image):
 				traffic_light_result.recognition_result = GREEN
 				green_detected = True
 
-	if not red_detected and not green_detected:
+	# if not red_detected and not green_detected:
+	if not green_detected :
 		traffic_light_result.recognition_result = UNKNOWN
 
-	roi_image.publish(cv_bridge.cv2_to_imgmsg(cimg, "bgr8"))
+	print(traffic_light_result.recognition_result)
+	cimg = cimg[50:80, 150:180]
+	# roi_image.publish(cv_bridge.cv2_to_imgmsg(cimg, "bgr8"))
+	roi_image.publish(cv_bridge.cv2_to_imgmsg(maskg))
 	light_detected_pub.publish(traffic_light_result)
 
 
@@ -163,9 +178,10 @@ if __name__ == '__main__':
 	detect_cnt = 0
 	traffic_light_result = traffic_light()
 	bounds = BBox(x=10, y=10, width=600, height=400)
-	predict_sub = rospy.Subscriber('/usb_cam/image_raw', Image, detect_callback, queue_size=1, buff_size=52428800)
+	# predict_sub = rospy.Subscriber('/usb_cam/image_raw', Image, detect_callback, queue_size=1, buff_size=52428800)
+	predict_sub = rospy.Subscriber('/usb_cam/image_raw', Image, detect_callback, queue_size=1)
 	light_detected_pub = rospy.Publisher(
-		'light_color', traffic_light, queue_size=5)
+		'light_color', traffic_light, queue_size=1)
 	roi_image = rospy.Publisher('roi_image', Image, queue_size=1)
 
 	rospy.spin()
